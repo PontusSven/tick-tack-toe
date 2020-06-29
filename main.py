584f301ee8db89cd905ec6adc2742c111590b24a
Pontussven <pontus.svensson92@gmail.com>
2020-06-29 14:33:51 +0200

    rework structure

--- main.py
+++ main.py
@@ -1,64 +1,51 @@
-game_list = [0,1,2]
-game_on = True
-status_list = ['Y', 'N']
+game_list = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',]
+acceptable_values = range(1,10)
+acceptable_insert = ['X', 'O']
 
+def start_up():
+    print('Let the games begin!')
+    print('Position indexes')
+    print('1', '2', '3')
+    print('4', '5', '6')
+    print('7', '8', '9')
 
 
 def display(game_list):
-    print('Here is the current list: ')
-    print(game_list)
+    print(game_list[0], game_list[1], game_list[2])
+    print(game_list[3], game_list[4], game_list[5])
+    print(game_list[6], game_list[7], game_list[8])
 
-def position_choice():
-    choice = 'wrong'
+def position():
+    position = input('Enter a position (1-9')
 
-    while choice not in game_list:
-        choice = input('Pick a postion (0,1,2)')
-
-        if choice not in game_list:
-            print('Invalid input')
-
-    return int(choice)
-
-def replacement_choice(game_list, position):
-
-    user_placement = input('Type a string to place at position: ')
-
-    game_list[position] = user_placement
-
-    return game_list
-
-    
-def gameon_choice():
-    choice = 'wrong'
-
-    while choice not in status_list:
-        choice = input('Keep playing? (Y or N) ')
-
-        if choice not in status_list:
-            print('Invalid input, please choose Y or N')
-
-    if choice == 'Y':
-        return True
+    while position not in acceptable_values:
+        position = input('Input not valid, enter a position (1-9')
     else:
-        print('Ending game...')
-        display(game_list)
-        return False
+        return int(position)
+
+def insert_value(position):
+    insert = input('Enter a value to insert (X or O)')
+    
+    while insert not in acceptable_insert:
+        insert = input('Input not valid, enter a value to insert (X or O)')
+
+    else:
+        game_list[position-1] = insert
+        return game_list
 
 def handler():
-    game_on = True
-    game_list = [0,1,2]
+    count = 0
+    start_up()
 
-    while game_on:
+    while count < 9:
+
+        user_position = position()
+        game_list = insert_value(user_position)
         display(game_list)
 
-        position = position_choice()
+        count += 1
 
-        game_list = replacement_choice(game_list, position)
-
-        display(game_list)
-
-        game_on = gameon_choice()
-    
-    
+    else:
+        print('Ending game...')
 
 handler()
